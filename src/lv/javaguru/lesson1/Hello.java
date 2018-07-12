b1711b3ec12ae9981ba2830223f1f020e6a684ca
ElMartinYo <martins.ceske@meawallet.com>
2018-07-12 19:23:54 +0300

    setup

--- src/lv/javaguru/lesson1/Hello.java
+++ src/lv/javaguru/lesson1/Hello.java
@@ -1,4 +1,14 @@
 package lv.javaguru.lesson1;
 
 public class Hello {
+
+    public static void main(String[] args) {
+
+        int someNumber = 12;
+
+//        System.out.println("Hello");
+        System.out.println(someNumber);
+
+    }
+
 }
